f410073182adf66b7a9a59c64f9692d078ce4a0f
Sahil Shaikh <104665337+Sahils1112@users.noreply.github.com>
2026-01-13 15:10:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,4 +29,7 @@ app.use("/api", (req, res, next) => {
 
 //define port
 
-app.listen(5001, () => console.log("app started at 5001..."));
+app.listen(5001, '0.0.0.0', () => {
+  console.log('Backend running on port 5001');
+});
+
